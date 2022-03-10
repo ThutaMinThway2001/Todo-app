2d3b97ce540812531dfa09fd140fb7bb7da79264
Thuta <thutaminthway2001@gmail.com>
2022-03-10 17:22:42 +0630

    breakpoints and visiblity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>This is home page</h1>
-    <v-btn depressed small class="indigo darken-4 white--text">
+    <v-btn depressed small class="indigo darken-4 white--text d-none d-sm-flex">
       <v-icon left small>
         email
       </v-icon>
