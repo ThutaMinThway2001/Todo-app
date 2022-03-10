3a737edeb33d89da3320a3ea828d31d5dbdfdd45
Thuta <thutaminthway2001@gmail.com>
2022-03-10 20:46:36 +0630

    Lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,14 +8,23 @@
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat color="grey">
+      <v-btn flat color="white">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="indigo">
-      <p>test</p>
+     <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -25,7 +34,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' },
+      ]
     }
   }
 }
