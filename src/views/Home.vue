bf17a1d041c7ceab23b352c7bea364f7602905f6
Thuta <thutaminthway2001@gmail.com>
2022-03-10 12:18:15 +0630

    text, typography and colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>This is home page</h1>
+    <p class="red white--text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit perspiciatis accusantium consectetur culpa, libero ratione cumque eveniet, amet ipsum quas vitae ea odit. Consectetur aperiam error libero neque quis? Iure?
+    </p>
+    <p class="pink lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, similique? Ipsum repudiandae quis esse ipsam. Est suscipit culpa facere rerum, odit quo labore, ipsum officia ex officiis cum. Eius, inventore.
+    </p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline.</p>
+    <p class="subheading">This is a subheading.</p>
+    <p class="caption font-weight-black">This is a caption.</p>
   </div>
 </template>
 
