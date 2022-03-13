314b5068a20ce60e2fadfc25fd8a128ae4ec2862
Thuta <thutaminthway2001@gmail.com>
2022-03-13 21:41:18 +0630

    form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -14,6 +14,14 @@
             <v-card-title>
                 <h2>Add a New Project</h2>
             </v-card-title>
+            <v-card-text>
+                <v-form class="px-3">
+                    <v-text-field v-model="title" label="title" prepend-icon="folder"></v-text-field>
+                    <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+                    <v-spacer></v-spacer>
+                    <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
+                </v-form>
+            </v-card-text>
             </v-card>
         </template>
       </v-dialog>
@@ -21,7 +29,17 @@
 
 <script>
 export default {
-
+    data(){
+        return{
+            title: '',
+            content: ''
+        }
+    },
+    methods: {
+        submit(){
+            alert('hi there');
+        }
+    }
 }
 </script>
 
