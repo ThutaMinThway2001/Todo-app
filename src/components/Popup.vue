a4fcf92d22a6f239e02ac6264146ac4584687147
Thuta <thutaminthway2001@gmail.com>
2022-03-14 11:42:31 +0630

    date picker

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -18,6 +18,14 @@
                 <v-form class="px-3">
                     <v-text-field v-model="title" label="title" prepend-icon="folder"></v-text-field>
                     <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+                        <!-- date  -->
+                            <v-menu max-width="290">
+                                <template v-slot:activator="{ on }">
+                                    <v-text-field :value="formattedDate" label="Due date" prepend-icon="mdi-calendar-range" v-on="on"></v-text-field>
+                                </template>
+                                <v-date-picker v-model="due"></v-date-picker>
+                            </v-menu>
+                        <!-- end date  -->
                     <v-spacer></v-spacer>
                     <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
                 </v-form>
@@ -28,16 +36,25 @@
 </template>
 
 <script>
+import format from 'date-fns/format';
+import parseISO from 'date-fns/esm/fp/parseISO';
 export default {
     data(){
         return{
             title: '',
-            content: ''
+            content: '',
+            due: null,
+            menu: false
         }
     },
     methods: {
         submit(){
             alert('hi there');
+        }
+    },
+    computed: {
+        formattedDate(){
+            return this.due ? format(parseISO(this.due), 'do MMM yyyy') : ''
         }
     }
 }
